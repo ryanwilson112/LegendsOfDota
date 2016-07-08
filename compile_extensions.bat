a48cd4c9ed5db686c1332dad4279d0816fc22ad9
ash47 <ash.fourtyseven@gmail.com>
2016-07-08 20:17:38 +1000

    Started to add LoD side of extension system

---
+++ compile_extensions.bat
@@ -0,0 +1,6 @@
+:: This file is only for loading and compiling experimental Legends of Dota Extensions
+:: There are currently no public extensions at this time
+
+:: Change into the directory and run the compiler
+cd import/LegendsOfDotaExtender/
+compile_extensions.bat
